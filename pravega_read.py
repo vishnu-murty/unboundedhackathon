2f407cb8657cd7a45b40c5ce623a9c2dd1ca468c
Thanuja_C <thanuja_c@dell.com>
2021-05-15 13:21:02 -0400

    Added decorator for ES validation + context driven variabes to make docker compatible

--- pravega_read.py
+++ pravega_read.py
@@ -4,31 +4,22 @@ import asyncio
 import time
 import requests
 from elasticsearch import Elasticsearch, helpers
-elastic_ip = "100.102.128.28"
-elastic_port = "8080"
-elastic_index = "pravega_telemetry"
-elastic_type = "_doc"
-elastic_url = "http://{}:{}/{}/{}".format(elastic_ip, elastic_port, elastic_index, elastic_type)
-pravega_ip = "100.64.25.48"
-pravega_port = "9090"
-manager = pravega_client.StreamManager("{}:{}".format(pravega_ip, pravega_port))
-reader_group = manager.create_reader_group("rg23","dell-scope5", "dell-stream5")
-reader1 = reader_group.create_reader("rdr6")
+import settings
+from utils.helper import es_connector
+
+
+manager = pravega_client.StreamManager("{}:{}".format(settings.pravega.host, settings.pravega.port))
+reader_group = manager.create_reader_group(settings.pravega_reader.group,settings.pravega_reader.scope, settings.pravega_reader.stream)
+reader1 = reader_group.create_reader(settings.pravega_reader.reader_id)
+@es_connector
 async def reader():
     slice = await reader1.get_segment_slice_async()
     for event in slice:
-       try:
-           requests.get("http://{}:{}".format(elastic_ip, elastic_port))
-           
-       except Exception as e:
-           #print(e)
-           continue
        str_data = event.data()
        json_list_data = json.loads(str_data)
-       #print(json_list_data)
        try:
-           elastic = Elasticsearch("{}:{}".format(elastic_ip,elastic_port))   
-           response = helpers.bulk(elastic, json_list_data, index=elastic_index)
+           elastic = Elasticsearch("{}:{}".format(settings.es.host,settings.es.port))   
+           response = helpers.bulk(elastic, json_list_data, index=settings.es.index)
        except Exception as e:
            #print(e)
            pass
