747b749ac537b9985e5e802fbf9e42ed53722d65
Thanuja_C <thanuja_c@dell.com>
2021-05-16 07:57:03 -0400

    added logic for getting online reader + added ES store to helper + requirement.txt

--- settings.py
+++ settings.py
@@ -9,17 +9,17 @@ def _(name, default, cast=str):
     return cast(os.environ.get(name, default))
 
 es = Context(
-    host=_("ELASTIC_HOST", "100.102.128.28"),
+    host=_("ELASTIC_HOST", "localhost"),
     port=_("ELASTIC_PORT", 8080, int),
     index=_("ELASTIC_INDEX", "pravega_telemetry")
 )
 
 
 pravega = Context(
-    host=_("PRAVEGA_HOST", "100.64.25.48"),
+    host=_("PRAVEGA_HOST", "localhost"),
     port=_("PRAVEGA_PORT", 9090, int),
-    scope=_("PRAVEGA_SCOPE", "dell-scope8"),
-    stream=_("PRAVEGA_STREAM", "dell-stream8"),
+    scope=_("PRAVEGA_SCOPE", "dell-scope9"),
+    stream=_("PRAVEGA_STREAM", "dell-stream9"),
     group=_("PRAVEGA_READER_GROUP", "rg0"),
     reader_id=_("PRAVEGA_READER_ID", "rdr")
 )
